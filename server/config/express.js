43a7f6dc7b58a19c56703e3aedd6518dd144e487
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-18 09:29:17 -0300

    removed connect reload

--- server/config/express.js
+++ server/config/express.js
@@ -42,17 +42,10 @@ module.exports = function (app) {
     }));
 
     if ('production' === env) {
-        app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-        app.use(express.static(path.join(config.root, 'public')));
-        app.set('appPath', config.root + '/public');
         app.use(morgan('dev'));
     }
 
     if ('development' === env || 'test' === env) {
-        app.use(require('connect-livereload')());
-        app.use(express.static(path.join(config.root, '.tmp')));
-        app.use(express.static(path.join(config.root, 'client')));
-        app.set('appPath', 'client');
         app.use(morgan('dev'));
         app.use(errorHandler()); // Error handler - has to be last
     }
